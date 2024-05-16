d1ef65d19f451fb654a993dbeff00421285cc1a5
ShanliuX <146153065+ShanliuX@users.noreply.github.com>
2024-05-16 21:28:32 +0800

    Update aiapi.py

--- aiapi.py
+++ aiapi.py
@@ -23,7 +23,7 @@ def generateChatResponse(prompt):
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
 
     try:
-        answer = completion.choices[0].message.content
+        answer = response.choices[0].message.content
     except:
         answer = 'Oops you beat the AI, try a different question, if the problem persists come back later.'
 
