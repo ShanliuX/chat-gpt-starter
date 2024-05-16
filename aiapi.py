0bfba54fe669220e30bd1248736e6cc6fa0be63d
ShanliuX <146153065+ShanliuX@users.noreply.github.com>
2024-05-16 16:45:00 +0800

    Update aiapi.py

--- aiapi.py
+++ aiapi.py
@@ -7,13 +7,19 @@ client = OpenAI()
 client.api_key = api_key
 
 def generateChatResponse(prompt):
+    messages = []
+    messages.append({"role": "system", "content": "You are a helpful assistant."})
 
-    response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-      ]
-    )
+    question = {}
+    question['role'] = 'user'
+    question['content'] = prompt
+
+    
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)
+
+    try:
+        answer = completion.choices[0].message.content
+    except:
+        answer = 'Oops you beat the AI, try a different question, if the problem persists come back later.'
+
+    return answer
